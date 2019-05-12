3116684e0034f741ba8f0162849c0cb51df31305
Anurag Soni <anurag@sonianurag.com>
2019-05-11 22:24:25 -0400

    Add more github routes

--- bench/github.ml
+++ bench/github.ml
@@ -29,10 +29,10 @@ let router =
     ; `DELETE, (fun _ _ -> "delete") <$> applications_root *> str </> s "tokens" *> str
     ]
   in
-  let users =
-    let user = s "user" in
-    let users = s "users" in
-    let emails = user *> s "emails" in
+  let user = s "user" in
+  let users = s "users" in
+  let emails = user *> s "emails" in
+  let users_routes =
     [ `GET, (fun _ -> "user") <$> users *> str
     ; `GET, "user" <$ user
     ; `GET, "users" <$ users
@@ -42,10 +42,37 @@ let router =
     ; `GET, "repos" <$ user *> s "repos"
     ]
   in
+  let events = s "events" in
+  let user_events = users *> str <* events in
+  let notifications = s "notifications" in
+  let notification_thread = notifications *> s "threads" *> int in
   let activity =
-    [ `GET, (fun _ _ -> "stars") <$> s "repos" *> str </> str <* s "stargazers" ]
+    [ `GET, "events" <$ events
+    ; `GET, (fun _ _ -> "repos") <$> s "repos" *> str </> str <* events
+    ; `GET, (fun _ -> "org") <$> s "orgs" *> str <* events
+    ; `GET, (fun _ -> "user") <$> users *> str <* s "received_events"
+    ; `GET, (fun _ -> "user") <$> users *> str <* s "received_events" <* s "public"
+    ; `GET, (fun _ -> "event") <$> user_events
+    ; `GET, (fun _ -> "public") <$> user_events <* s "public"
+    ; `GET, (fun _ _ -> "org event") <$> user_events </> s "orgs" *> str
+    ; `GET, "feeds" <$ s "feeds"
+    ; `GET, "notifications" <$ notifications
+    ; `GET, (fun _ _ -> "repo notif") <$> s "repos" *> str </> str <* notifications
+    ; `PUT, "notif" <$ notifications
+    ; `PUT, (fun _ _ -> "repo notif") <$> s "repos" *> str </> str <* notifications
+    ; `GET, (fun _ -> "thread id") <$> notification_thread
+    ; `GET, (fun _ -> "put thread") <$> notification_thread <* s "subscription"
+    ; `PUT, (fun _ -> "put thread") <$> notification_thread <* s "subscription"
+    ; `DELETE, (fun _ -> "put thread") <$> notification_thread <* s "subscription"
+    ; `GET, (fun _ _ -> "stars") <$> s "repos" *> str </> str <* s "stargazers"
+    ; `GET, (fun _ -> "starred") <$> users *> str <* s "starred"
+    ; `GET, "starred" <$ user *> s "starred"
+    ; `GET, (fun _ _ -> "owner") <$> user *> s "starred" *> str </> str
+    ; `PUT, (fun _ _ -> "owner") <$> user *> s "starred" *> str </> str
+    ; `DELETE, (fun _ _ -> "owner") <$> user *> s "starred" *> str </> str
+    ]
   in
-  with_method @@ List.concat [ auth; applications; users; activity ]
+  with_method @@ List.concat [ auth; applications; users_routes; activity ]
 ;;
 
 open Core_bench
