64b8368de8ab72d84f7cb0ff81aaff34fa258ef9
Anurag Soni <anurag@sonianurag.com>
2020-01-19 10:53:09 -0500

    add trie + method module

--- src/routes.ml
+++ src/routes.ml
@@ -1,3 +1,103 @@
+module Method = struct
+  module T = struct
+    type standard =
+      [ `GET
+      | `HEAD
+      | `POST
+      | `PUT
+      | `DELETE
+      | `CONNECT
+      | `OPTIONS
+      | `TRACE
+      ]
+
+    type t =
+      [ standard
+      | `Other of string
+      ]
+
+    let default = `GET
+
+    let to_string = function
+      | `GET -> "GET"
+      | `HEAD -> "HEAD"
+      | `POST -> "POST"
+      | `PUT -> "PUT"
+      | `DELETE -> "DELETE"
+      | `CONNECT -> "CONNECT"
+      | `OPTIONS -> "OPTION"
+      | `TRACE -> "TRACE"
+      | `Other s -> s
+
+    let compare m1 m2 = String.compare (to_string m1) (to_string m2)
+    let pp fmt m = Format.fprintf fmt "%s" (to_string m)
+    let equal m1 m2 = compare m1 m2 = 0
+  end
+
+  include T
+  module M = Map.Make (T)
+end
+
+module PatternTrie = struct
+  module Key = struct
+    type t =
+      | Match : string -> t
+      | Capture : t
+  end
+
+  module KeyMap = Map.Make (String)
+
+  type 'a node =
+    { parsers : 'a list
+    ; children : 'a node KeyMap.t
+    ; capture : 'a node option
+    }
+
+  type 'a t = 'a node
+
+  let empty = { parsers = []; children = KeyMap.empty; capture = None }
+
+  let feed_params t params =
+    let rec aux t params captures =
+      match t, params with
+      | { parsers = []; _ }, [] -> [], []
+      | { parsers = rs; _ }, [] -> rs, List.rev captures
+      | { children; capture; _ }, x :: xs ->
+        (match KeyMap.find_opt x children with
+        | None ->
+          (match capture with
+          | None -> [], []
+          | Some t' -> aux t' xs (x :: captures))
+        | Some m' -> aux m' xs captures)
+    in
+    aux t params []
+
+  let add k v t =
+    let rec aux k t =
+      match k, t with
+      | [], ({ parsers = x; _ } as n) -> { n with parsers = v :: x }
+      | x :: r, ({ children; capture; _ } as n) ->
+        (match x with
+        | Key.Match w ->
+          let t' =
+            match KeyMap.find_opt w children with
+            | None -> empty
+            | Some v -> v
+          in
+          let t'' = aux r t' in
+          { n with children = KeyMap.add w t'' children }
+        | Key.Capture ->
+          let t' =
+            match capture with
+            | None -> empty
+            | Some v -> v
+          in
+          let t'' = aux r t' in
+          { n with capture = Some t'' })
+    in
+    aux k t
+end
+
 type 'a conv =
   { to_ : 'a -> string
   ; from_ : string -> 'a option
@@ -5,25 +105,26 @@ type 'a conv =
 
 let conv to_ from_ = { to_; from_ }
 
-type ('a, 'b) t =
-  | End : ('a, 'a) t
-  | Match : string * ('a, 'b) t -> ('a, 'b) t
-  | Conv : 'c conv * ('a, 'b) t -> ('c -> 'a, 'b) t
+type ('a, 'b) path =
+  | End : ('a, 'a) path
+  | Match : string * ('a, 'b) path -> ('a, 'b) path
+  | Conv : 'c conv * ('a, 'b) path -> ('c -> 'a, 'b) path
 
-and ('a, 'b) req = Req : string * ('a, 'b) t -> ('a, 'b) req
+and ('a, 'b) req = Req : Method.t * ('a, 'b) path -> ('a, 'b) req
 
 type 'b route = Route : ('a, 'b) req * 'a -> 'b route
 
 let route r handler = Route (r, handler)
-let ( ==> ) = route
+let ( @--> ) = route
 let s w r = Match (w, r)
 let of_conv conv r = Conv (conv, r)
 let int r = of_conv (conv string_of_int int_of_string_opt) r
 let str r = of_conv (conv Fun.id (fun (x : string) -> Some x)) r
 let bool r = of_conv (conv string_of_bool bool_of_string_opt) r
-let ( </> ) m1 m2 r = m1 @@ m2 r
+let ( / ) m1 m2 r = m1 @@ m2 r
+let nil = End
 
-let rec print_params : type a b. (string -> b) -> (a, b) t -> a =
+let rec print_params : type a b. (string -> b) -> (a, b) path -> a =
  fun k -> function
   | End -> k ""
   | Match (w, fmt) -> print_params (fun s -> k @@ String.concat "/" [ w; s ]) fmt
@@ -31,13 +132,16 @@ let rec print_params : type a b. (string -> b) -> (a, b) t -> a =
     let f x = print_params (fun str -> k @@ String.concat "/" [ to_ x; str ]) fmt in
     f
 
-let rec print_pattern : type a b. (a, b) t -> string = function
+let rec print_pattern : type a b. (a, b) path -> string = function
   | End -> ""
   | Match (w, fmt) -> w ^ "/" ^ print_pattern fmt
   | Conv (_, fmt) -> ":capture/" ^ print_pattern fmt
 
+let sprintf r = print_params Fun.id r
+let pp fmt r = Format.fprintf fmt "%s" (print_pattern r)
+
 let parse_route fmt handler params =
-  let rec match_target : type a b. (a, b) t -> a -> string list -> b option =
+  let rec match_target : type a b. (a, b) path -> a -> string list -> b option =
    fun t f s ->
     match t with
     | End -> Some f
--- src/routes.mli
+++ src/routes.mli
@@ -1,12 +1,50 @@
-type ('a, 'b) t
+(** Typed routing for OCaml.
+    [Routes] provides combinators for adding typed routing
+    to OCaml applications. The core library will be independent
+    of any particular web framework or runtime.
+*)
+
+module Method : sig
+  type standard =
+    [ `GET
+    | `HEAD
+    | `POST
+    | `PUT
+    | `DELETE
+    | `CONNECT
+    | `OPTIONS
+    | `TRACE
+    ]
+
+  type t =
+    [ standard
+    | `Other of string
+    ]
+  (** HTTP methods. This is an optional input for route matching.
+      The current types are chosen to be compatible with what Httpaf uses - {{:https://github.com/inhabitedtype/httpaf/blob/c2ee924eaccd2adb2e6aea0b9bc6a0ffe6132723/lib/method.ml} link}. *)
+
+  val pp : Format.formatter -> t -> unit
+  (** @since 0.6.0 *)
+
+  val equal : t -> t -> bool
+  (** @since 0.6.0 *)
+
+  val compare : t -> t -> int
+  (** @since 0.6.0 *)
+end
+
+type ('a, 'b) path
 type ('a, 'b) req
 type 'b route
 
-val int : ('a, 'b) t -> (int -> 'a, 'b) t
-val str : ('a, 'b) t -> (string -> 'a, 'b) t
-val bool : ('a, 'b) t -> (bool -> 'a, 'b) t
-val s : string -> ('a, 'b) t -> ('a, 'b) t
-val ( </> ) : (('a, 'b) t -> 'c) -> ('d -> ('a, 'b) t) -> 'd -> 'c
-val ( ==> ) : ('a, 'b) req -> 'a -> 'b route
-val meth' : string -> (('a, 'a) t -> ('b, 'c) t) -> ('b, 'c) req
+val int : ('a, 'b) path -> (int -> 'a, 'b) path
+val str : ('a, 'b) path -> (string -> 'a, 'b) path
+val bool : ('a, 'b) path -> (bool -> 'a, 'b) path
+val s : string -> ('a, 'b) path -> ('a, 'b) path
+val ( / ) : (('a, 'b) path -> 'c) -> ('d -> ('a, 'b) path) -> 'd -> 'c
+val ( @--> ) : ('a, 'b) req -> 'a -> 'b route
+val meth' : Method.t -> (('a, 'a) path -> ('b, 'c) path) -> ('b, 'c) req
 val match' : 'a route list -> string -> 'a option
+val sprintf : ('a, string) path -> 'a
+val pp : (Format.formatter -> ('a, 'b) path -> unit[@ocaml.toplevel_printer])
+val nil : ('a, 'a) path
