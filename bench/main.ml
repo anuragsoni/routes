a0d15fb5879e90c4af4e34f653b188dde7688360
Anurag Soni <anurag@sonianurag.com>
2020-01-18 23:04:05 -0500

    remove more content

--- bench/main.ml
+++
@@ -1,9 +0,0 @@
-open Core
-open Core_bench
-
-let () =
-  let benches =
-    List.concat [ [ Static.bench ]; Github.github_benches; Parse.parse_benches ]
-  in
-  Command.run @@ Bench.make_command benches
-;;
