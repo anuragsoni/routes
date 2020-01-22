19dfad6783c2135d89466606af3e0855dbd79113
Anurag Soni <anurag@sonianurag.com>
2020-01-21 20:53:52 -0500

    fix example

--- example/no_method.ml
+++ example/no_method.ml
@@ -6,15 +6,15 @@ module R = struct
   let admin_handler a = if a then "User is admin" else "User is not an admin"
   let route r = None, r
   let user () = s "user"
-  let user_and_id () = user () / int /? nil
-  let user_and_admin () = user () / bool /? nil
-  let q () = s "confusing" /? nil
+  let user_and_id = user () / int /? nil
+  let user_and_admin = user () / bool /? nil
+  let q = s "confusing" /? nil
 
   let routes =
     one_of
-      [ route @@ (fun () -> s "hi" /? nil) @--> "Hello, World"
-      ; route @@ (fun () -> s "hello" / s "from" / s "routes" /? nil) @--> "Hello, Routes"
-      ; route @@ (fun () -> s "sum" / int / int /? nil) @--> sum
+      [ route @@ (s "hi" /? nil) @--> "Hello, World"
+      ; route @@ (s "hello" / s "from" / s "routes" /? nil) @--> "Hello, Routes"
+      ; route @@ (s "sum" / int / int /? nil) @--> sum
       ; route @@ user_and_id @--> id_handler
       ; route @@ user_and_admin @--> admin_handler
       ; route @@ q @--> "Foobar"
